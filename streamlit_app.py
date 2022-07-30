eecc405e80ca02031232d3fced36190c75edea9a
preetigupta0703 <110299123+preetigupta0703@users.noreply.github.com>
2022-07-30 12:10:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 
 import streamlit
+import pandas
 
 streamlit.title("My parents new healthy diner")
 streamlit.header("Main Menu")
 streamlit.text("🥣 Omega 3 cereal")
 streamlit.text("🥬 kale smoothie")
 streamlit.text("🥗 salad")
+
+my_furit_salad = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_salad)
