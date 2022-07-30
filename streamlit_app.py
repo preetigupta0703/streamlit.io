425ed3397c8a3bf5aa3b2abc2b7ae798ef1c1f8b
preetigupta0703 <110299123+preetigupta0703@users.noreply.github.com>
2022-07-30 12:07:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 
 streamlit.title("My parents new healthy diner")
 streamlit.header("Main Menu")
-streamlit.text("Omega 3 cereal")
-streamlit.text("kale smoothie")
-streamlit.text("salad")
+streamlit.text("🥣 Omega 3 cereal")
+streamlit.text("🥬 kale smoothie")
+streamlit.text("🥗 salad")
